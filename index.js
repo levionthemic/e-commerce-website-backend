ccfc9b0700f1b68547c5411f91f516d23ab5e731
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-12-13 11:14:00 +0700

    deploy

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+require("dotenv").config();
+
+const bodyParser = require("body-parser");
+
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const database = require("./config/database");
+
+const adminRoutes = require("./api/v1/routes/admin/index.route");
+const clientRoutes = require("./api/v1/routes/client/index.route");
+const sellerRoutes = require("./api/v1/routes/seller/index.route");
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// Database
+database.connect();
+
+const corsOptions = {
+  origin: "https://e-commerce-website-client-steel.vercel.app",
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
+
+app.use(cors(corsOptions));
+
+// Routes
+adminRoutes(app);
+clientRoutes(app);
+sellerRoutes(app);
+
+// Listen
+app.listen(port, () => {
+  console.log(`App listen on port ${port}`);
+});
